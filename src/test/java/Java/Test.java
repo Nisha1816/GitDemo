f098380db23c606a9dfa483d575fc850145c3631
Nisha1816 <nishaptn@gmail.com>
2023-12-07 18:13:36 +0530

    Done by GitY

--- src/test/java/Java/Test.java
+++ src/test/java/Java/Test.java
@@ -2,6 +2,9 @@ package Java;
 class Test {
 		public static void main(String []args){
 		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 		System.out.println("Let's Print All the arguments");
 		for(int i=0;i<args.length;i++)  
 		System.out.println(args[i]);  
