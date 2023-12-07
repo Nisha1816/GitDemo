b77d8643e3be439fd1e59d60928fb9420c6c4bd7
Nisha1816 <nishaptn@gmail.com>
2023-12-07 18:53:00 +0530

    Done Today

--- src/test/java/Java/Test.java
+++ src/test/java/Java/Test.java
@@ -6,6 +6,8 @@ class Test {
 		System.out.println("Hello");
 		System.out.println("Hello");
 		System.out.println("Let's Print All the arguments");
+		System.out.println("Let's Print All the arguments");
+		System.out.println("Let's Print All the arguments");
 		for(int i=0;i<args.length;i++)  
 		System.out.println(args[i]);  
 		}
